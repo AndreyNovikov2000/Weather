89a1ec065c3aef68aa88997de5c6fb68af67ce4e
Andrey Novikov <andrenovikov2000@mail.ru>
2020-01-25 18:35:42 +0600

    2.0) Update open weather map

--- Weather/Model/OpenWeatherMap.swift
+++ Weather/Model/OpenWeatherMap.swift
@@ -6,16 +6,64 @@
 //  Copyright © 2020 Andrey Novikov. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct OpenWeatherMap {
     let nameCity: String
     let temp: Double
+    let description: String
+    let currentTime: Int
+    let icon: String
     
     init(weatherJson: NSDictionary) {
         nameCity = weatherJson["name"] as? String ?? ""
         
         let mainWeatherJSON = weatherJson["main"] as? [String: AnyObject]
         temp = mainWeatherJSON?["temp"] as? Double ?? 1.0
+        
+        let weather = weatherJson["weather"] as? [[String: AnyObject]]
+        let firstIndexOfWeather = weather?[0]
+        description = firstIndexOfWeather?["description"] as? String ?? ""
+        
+        currentTime = weatherJson["dt"] as? Int ?? 0
+        
+        icon = firstIndexOfWeather?["icon"] as? String ?? ""
+    }
+    
+    func getTimeFromUnix(unixTime: Int) -> String {
+        let timeInSecond = TimeInterval(unixTime)
+        let weatherDate = Date(timeIntervalSince1970: timeInSecond)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        return dateFormatter.string(from: weatherDate)
+    }
+    
+    func getWetherIcon(stringIcon: String) -> UIImage {
+        var imageName: String
+        
+        switch stringIcon {
+        case "01d": imageName = "01d"
+        case "02d": imageName = "02d"
+        case "03d": imageName = "03d"
+        case "04d": imageName = "04d"
+        case "09d": imageName = "09d"
+        case "10d": imageName = "10d"
+        case "11d": imageName = "11d"
+        case "13d": imageName = "13d"
+        case "50d": imageName = "50d"
+        case "01n": imageName = "01n"
+        case "02n": imageName = "02n"
+        case "03n": imageName = "03n"
+        case "04n": imageName = "04n"
+        case "09n": imageName = "09n"
+        case "10n": imageName = "10n"
+        case "11n": imageName = "11n"
+        case "13n": imageName = "13n"
+        case "50n": imageName = "50n"
+        default: imageName = "none"
+        }
+        
+        return UIImage(named: imageName)!
     }
 }
--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,44 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
+    @IBOutlet weak var imageView: UIImageView!
+    private let url = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let urlString = URL(string: url)!
-        let data = try! Data.init(contentsOf: urlString)
-        print(data)
         
         let session = URLSession.shared
-        //Async thread in main
         session.downloadTask(with: urlString) { (location, response, error) in
             if let error = error {
-                print(error)
+                print(error.localizedDescription)
             }
             
             let weatherData = try! Data.init(contentsOf: urlString)
             let weatherJSON = try! JSONSerialization.jsonObject(with: weatherData, options: [])
             let jsonDictinary = weatherJSON as! NSDictionary
-            print(jsonDictinary)
             
             let weather = OpenWeatherMap(weatherJson: jsonDictinary)
             print("let nameCity = ", weather.nameCity)
             print("let temp = ", weather.temp)
+            print("let description = ", weather.description)
+            print("let currentTime =", weather.currentTime)
+            print("let icon = ", weather.icon)
             
-            
-            if let cityName = jsonDictinary["name"] as? String {
-                print("CITY:  ",cityName)
+            DispatchQueue.main.async {
+                self.imageView.image = weather.getWetherIcon(stringIcon: weather.icon)
             }
             
-            if let main = jsonDictinary["main"] as? [String: AnyObject] {
-                if let temp = main["temp"] as? Double {
-                    print("TEMP: ", temp)
-                }
-            }
-            
-            
-           
             
         }.resume()
     }
